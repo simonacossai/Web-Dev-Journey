b303f98b140512bc0347a11c924ff0127d659e3e
Simona Cossai <simonacossai@strive.school>
2020-04-24 18:03:45 +0200

    DONE- changing navbar features

--- Netflix/js/SecondPage.js
+++ Netflix/js/SecondPage.js
@@ -17,3 +17,19 @@ function changeImage2() {
   document.getElementById('foreground').src = "img/catmovie2.jpg";
 
 }
+
+$(document).ready(function(){       
+  var scroll_start = 0;
+  var startchange = $('#startchange');
+  var offset = startchange.offset();
+   if (startchange.length){
+  $(document).scroll(function() { 
+     scroll_start = $(this).scrollTop();
+     if(scroll_start > offset.top) {
+         $("#nav").css('background-color', '#f0f0f0');
+      } else {
+         $('#nav').css('background-color', 'transparent');
+      }
+  });
+   }
+});
