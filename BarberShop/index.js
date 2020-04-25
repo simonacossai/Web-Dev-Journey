df3d88425bc6659e507f3d2cceba47c66195e89d
Simona Cossai <simonacossai@strive.school>
2020-04-26 01:13:26 +0200

    WIP- slide animation

---
+++ BarberShop/index.js
@@ -0,0 +1,9 @@
+$('.carousel-inner').superslides(options_hash)
+delay: 4000
+play: false
+slide_speed: 'normal'
+slide_easing: 'linear'
+nav_class: 'carousel-innern'
+container_class: 'carousel-inner'
+pagination: true
+hashchange: true
