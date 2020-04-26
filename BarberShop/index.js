1b265db9972c5a55f5a135304601881acb210467
Simona Cossai <simonacossai@strive.school>
2020-04-26 17:24:47 +0200

    DONE- add superslides animation to images carousel

--- BarberShop/index.js
+++
@@ -1,9 +0,0 @@
-$('.carousel-inner').superslides(options_hash)
-delay: 4000
-play: false
-slide_speed: 'normal'
-slide_easing: 'linear'
-nav_class: 'carousel-innern'
-container_class: 'carousel-inner'
-pagination: true
-hashchange: true
