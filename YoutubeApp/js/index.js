7f2083f56c9c1b7a913b540785619316f0e4496a
Simona Cossai <simonacossai@strive.school>
2020-05-01 18:10:32 +0200

    DONE- add videos functions

--- YoutubeApp/js/index.js
+++ YoutubeApp/js/index.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 
-    let key= "AIzaSyAIhkXJnuDnLMq0FCtoLiRaC25Sx6iutxw";
-    let playlistId = "PLBeeiCx5Kc2s-gOu3U0awfa0NqOvU6v9Q";
-    let URL ="https://www.googleapis.com/youtube/v3/playlistItems";
+    var key= "AIzaSyAIhkXJnuDnLMq0FCtoLiRaC25Sx6iutxw"; 
+    var playlistId = "PLBeeiCx5Kc2s-gOu3U0awfa0NqOvU6v9Q";
+    var URL ="https://www.googleapis.com/youtube/v3/playlistItems";
 
     let options = {
         part: 'snippet',
@@ -14,14 +14,39 @@ $(document).ready(function(){
     
     function loadVids(){
         $.getJSON(URL, options, function(data){
-            console.log(data);
-            mainVid();
+            var id = data.items[0].snippet.resourceId.videoId;
+            mainVid(id);
+            resultsLoop(data);
         })
     }
 
-    function mainVid() {
+    function mainVid(id) {
         $('#video').html(`
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/6BPDktJNBwE" frameborder="0" allow="accelerometer; autoplay;
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="accelerometer; autoplay;
           encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`);
     }
+
+    function resultsLoop(data) {
+        $.each(data.items, function(i, item){
+            var thumb = item.snippet.thumbnails.medium.url;
+            var title = item.snippet.title;
+            var desc = item.snippet.description.substring(0,100);
+            var vid = item.snippet.resourceId.videoId;
+            $('main').append(`
+            <article class="item" data-key="${vid}">
+
+                <img src="${thumb}" alt="" class="thumb">
+                <div class="details">
+                    <h4>${title}</h4>
+                    <p>${desc}</p>
+                </div>
+
+            </article>
+            `);
+        });   
+    }
+    $('main').on('click', 'article', function(){
+        var id = $(this).attr('data-key');
+        mainVid(id);
+});
 });
