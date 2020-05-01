81041a49d450156e9ec8c1d590ac36e61859ef06
Simona Cossai <simonacossai@strive.school>
2020-05-02 00:16:28 +0200

    ADD- background and empty carousel

---
+++ YoutubeAppMyVersion/js/index.js
@@ -0,0 +1,13 @@
+
+    $('.post-wrapper').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        nextArrow: $('.next'),
+        prevArrow: $('.prev'),
+      });
+
+
+
+
